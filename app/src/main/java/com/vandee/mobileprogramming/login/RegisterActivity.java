f2d8df23209893e52b2f209a24531dc3582cfb17
fandi21 <fandi.smart21@gmail.com>
2023-05-13 14:12:50 +0700

    approve

---
+++ app/src/main/java/com/vandee/mobileprogramming/login/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.vandee.mobileprogramming.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vandee.mobileprogramming.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
